c004943a162670049d1f05fc43e8bbff04b19937
dlawhdgus <212whdgus@gmail.com>
2022-10-27 15:44:07 +0900

    hide api key

--- config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-    API_CONNECT_KEY : "RGAPI-3e733b44-6296-447c-8fe2-bb29c0a0cea9",
-    DISCORD_KEY : "MTAzNDM1MzUyMTA2MTAwNzQwMg.GmgxCC.RDwL_bwIghPjIKZNghgVEkU3ggCTtaPr8QvHMQ",
-    PERFIX : '!'
-}
-
-module.exports = config
